ad30b7d2a58a17e2f0cee15637889867cf310428
aravindmeyappan <67819973+aravindmeyappan@users.noreply.github.com>
2024-01-13 23:18:08 +0530

    Added a separate class for linear SVM
    Added a linear_svm_classifier class in a separate python file and modified the calculation of the gradients a little bit

---
+++ linear_svm_classifier.py
@@ -0,0 +1,57 @@
+import numpy as np 
+
+class linear_svm_classifier():
+
+    def __init__(self, learning_rate, num_iter, reg_lambda):
+        self.learning_rate = learning_rate
+        self.num_iter = num_iter
+        self.reg_lambda = reg_lambda
+    
+    def fit(self, X, Y):
+
+        # getting the shape of x in order to find out the dimension of weights to initialize
+        self.m, self.n = X.shape # m --> number of examples, n --> number of features in input data
+
+        # initialize weights
+        self.w = np.zeros(self.n) # w is a column vector of dimension n
+        self.b = 0 # bias can be assigned a scalar and while computation it can be broadcasted according to dimension of w.T*x
+        self.X = X
+        self.Y = Y
+
+        # gradient descent implementation
+        for i in range(self.num_iter):
+            self.update_weights()
+
+    def update_weights(self):
+
+        ## Label Encoding
+        # In svm classifier instead of the traditional approach of giving 0 to absence and 1 to presence of a phenomena
+        # we will encode it to 1 for presence and -1 for absence.
+        y_label = np.where(self.Y<=0, -1, 1)
+
+        # initialize gradients
+        dw = np.zeros(self.n)
+        db = 0
+
+        # computing gradients at each pass
+        for idx,xi in enumerate(self.X): # iterating through each example(n dimensional vector) and its index (to check label)
+            condition = y_label[idx]*(np.dot(xi,self.w)-self.b) >= 1
+            if not condition:
+                dw += -np.dot(xi,y_label[idx]) 
+                db += y_label[idx]
+
+        # normalize the weights
+        dw /= self.m
+
+        # add the regularization 
+        dw += 2*self.reg_lambda*self.w
+
+        # update the parameters
+        self.w -= self.learning_rate*dw
+        self.b -= self.learning_rate*db
+
+    def predict(self, X):
+        output = np.dot(X, self.w) - self.b
+        predicted_labels = np.sign(output)
+        y_hat = np.where(predicted_labels<=-1, 0, 1)
+        return y_hat
